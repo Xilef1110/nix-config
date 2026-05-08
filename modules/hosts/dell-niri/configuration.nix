f0e910ece683740a29e60e87c4fcad512df80485
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-08 16:53:32 -0700

    Fix config errors

--- modules/hosts/dell-niri/configuration.nix
+++ modules/hosts/dell-niri/configuration.nix
@@ -8,7 +8,7 @@
         # Include the results of the hardware scan.
         self.nixosModules.myMachineHardware
         self.nixosModules.myHomeManager
-        self.nixosModules.dellNiriHomeModule
+        # self.nixosModules.dellNiriHomeModule
         self.nixosModules.niri
       ];
 
@@ -102,7 +102,7 @@
         "nix-command"
         "flakes"
       ];
-      services.power-proflies-daemon.enable = true;
+      services.power-profiles-daemon.enable = true;
       services.upower.enable = true;
 
       # This value determines the NixOS release from which the default
