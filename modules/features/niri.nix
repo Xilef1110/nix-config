40a896d8e66697d8056a84faac2ff2bace1bb33a
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-08 18:05:57 -0700

    Add Noctalia/Niri config

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -20,20 +20,69 @@
       packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
         inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
         settings = {
-          # spawn-at-startup = [
-          #   (lib.getExe self'.packages.myNoctalia)
-          # ];
+          spawn-at-startup = [
+            (lib.getExe self'.packages.myNoctalia)
+          ];
 
           xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 
           input.keyboard.xkb.layout = "us,ua";
 
           layout.gaps = 5;
+          touchpad = {
+            natural-scroll = null;
+            tap = null;
+          };
 
           binds = {
             "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
             "Mod+Q".close-window = null;
-            # "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+            "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+
+            # Window Navigation
+            "Mod+O".toggle-overwiew = null;
+
+            "Mod+F".maximize-column = null;
+            "Mod+G".fullscreen-window = null;
+            "Mod+Shift+F".toggle-window-floating = null;
+            "Mod+C".center-column = null;
+
+            "Mod+H".focus-column-left = null;
+            "Mod+L".focus-column-right = null;
+            "Mod+K".focus-window-up = null;
+            "Mod+J".focus-window-down = null;
+
+            "Mod+Left".focus-column-left = null;
+            "Mod+Right".focus-column-right = null;
+            "Mod+Up".focus-window-up = null;
+            "Mod+Down".focus-window-down = null;
+
+            "Mod+Shift+H".move-column-left = null;
+            "Mod+Shift+L".move-column-right = null;
+            "Mod+Shift+K".move-window-up = null;
+            "Mod+Shift+J".move-window-down = null;
+
+            "Mod+1".focus-workspace = "w0";
+            "Mod+2".focus-workspace = "w1";
+            "Mod+3".focus-workspace = "w2";
+            "Mod+4".focus-workspace = "w3";
+            "Mod+5".focus-workspace = "w4";
+            "Mod+6".focus-workspace = "w5";
+            "Mod+7".focus-workspace = "w6";
+            "Mod+8".focus-workspace = "w7";
+            "Mod+9".focus-workspace = "w8";
+            "Mod+0".focus-workspace = "w9";
+
+            "Mod+Shift+1".move-column-to-workspace = "w0";
+            "Mod+Shift+2".move-column-to-workspace = "w1";
+            "Mod+Shift+3".move-column-to-workspace = "w2";
+            "Mod+Shift+4".move-column-to-workspace = "w3";
+            "Mod+Shift+5".move-column-to-workspace = "w4";
+            "Mod+Shift+6".move-column-to-workspace = "w5";
+            "Mod+Shift+7".move-column-to-workspace = "w6";
+            "Mod+Shift+8".move-column-to-workspace = "w7";
+            "Mod+Shift+9".move-column-to-workspace = "w8";
+            "Mod+Shift+0".move-column-to-workspace = "w9";
           };
         };
       };
