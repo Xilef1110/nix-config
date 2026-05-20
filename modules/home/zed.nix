ac435047963669869f981a53a3dda960cad5ba06
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-19 20:27:48 -0700

    Add zed home  module

---
+++ modules/home/zed.nix
@@ -0,0 +1,27 @@
+{ self, inputs, ... }:
+{
+
+  flake.homeModules.dellNiriHomeModule =
+    { pkgs, lib, ... }:
+    {
+
+      programs.zed-editor = {
+        enable = true;
+        extensions = [
+          "nix"
+          "toml"
+          "rust"
+        ];
+        userSettings = {
+          theme = {
+            mode = "system";
+            dark = "One Dark";
+            light = "One Light";
+          };
+          hour_format = "hour24";
+          helix_mode = true;
+          assistant.enabled = false;
+        };
+      };
+    };
+}
