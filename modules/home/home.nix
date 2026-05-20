d2090df41eaec62cb0283a876d38a888e1f96c43
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-19 18:15:13 -0700

    Remove duplicate config in home manager

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -23,30 +23,30 @@
       programs.bash.shellAliases.ll = "ls -l";
 
       home.packages = [ pkgs.hello ];
-      programs.helix = {
-        enable = true;
-        settings = {
-          keys = {
-            normal = {
-              "C-s" = ":w";
-            };
-            insert = {
-              "A-i" = "normal_mode";
-            };
-          };
-          editor = {
-            soft-wrap.enable = true;
-            line-number = "relative";
-          };
-        };
-        languages.language = [
-          {
-            name = "nix";
-            auto-format = true;
-            # formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
-          }
-        ];
-      };
+      # programs.helix = {
+      #   enable = true;
+      #   settings = {
+      #     keys = {
+      #       normal = {
+      #         "C-s" = ":w";
+      #       };
+      #       insert = {
+      #         "A-i" = "normal_mode";
+      #       };
+      #     };
+      #     editor = {
+      #       soft-wrap.enable = true;
+      #       line-number = "relative";
+      #     };
+      #   };
+      #   languages.language = [
+      #     {
+      #       name = "nix";
+      #       auto-format = true;
+      #       # formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+      #     }
+      #   ];
+      # };
 
       programs.onedrive = {
         enable = true;
