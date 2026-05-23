b7d5c772c7acfc04957dff3aca12009ec2eff4bf
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-22 18:25:44 -0700

    Fix kitty config

--- modules/features/kitty.nix
+++ modules/features/kitty.nix
@@ -32,7 +32,7 @@
         settings = {
           enable_audio_bell = "no";
 
-          font_size = 15;
+          font_size = 12;
           font_family = "JetBrainsMono Nerd Font";
 
           cursor_text_color = "background";
@@ -42,22 +42,11 @@
 
           cursor_trail = 3;
 
-          map = [
-            "alt+1 goto_tab 1"
-            "alt+2 goto_tab 2"
-            "alt+3 goto_tab 3"
-            "alt+4 goto_tab 4"
-            "alt+5 goto_tab 5"
-            "alt+6 goto_tab 6"
-            "alt+7 goto_tab 7"
-            "alt+8 goto_tab 8"
-            "alt+9 goto_tab 9"
-            "ctrl+shift+w close_tab"
-            "ctrl+t new_tab_with_cwd"
-            "ctrl+shift+t new_tab"
-          ];
-
+          # Background
           background = self.theme.base00;
+          background_opacity = 0.9;
+          background_blur = 0.2;
+
           foreground = self.theme.base07;
 
           cursor = self.theme.base07;
@@ -65,10 +54,14 @@
           selection_foreground = self.theme.base02;
           selection_background = self.theme.base01;
 
+          # Tab managment
+          tab_bar_edge = "top";
+          tab_bar_style = "slant";
           active_tab_foreground = self.theme.base0B;
           active_tab_background = self.theme.base03;
           inactive_tab_background = self.theme.base01;
 
+          # import themes
           color0 = self.theme.base00;
           color8 = self.theme.base02;
           color1 = self.theme.base08;
@@ -85,6 +78,20 @@
           color14 = self.theme.base0C;
           color7 = self.theme.base03;
           color15 = self.theme.base03;
+        };
+        keybindings = {
+          "alt-1" = "goto_tab 1";
+          "alt-2" = "goto_tab 2";
+          "alt-3" = "goto_tab 3";
+          "alt-4" = "goto_tab 4";
+          "alt-5" = "goto_tab 5";
+          "alt-6" = "goto_tab 6";
+          "alt-7" = "goto_tab 7";
+          "alt-8" = "goto_tab 8";
+          "alt-9" = "goto_tab 9";
+          "ctrl+shift+w" = "close_tab";
+          "ctrl+t" = "new_tab_with_cwd";
+          "ctrl+shift+t" = "new_tab";
         };
       };
     };
