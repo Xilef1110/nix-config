d32dbb674fbdf88c62a9beabda3ecedd1fb9d5ad
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-25 15:35:40 -0700

    Add: Firefox extensions

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -38,6 +38,33 @@
             SuspectedFingerprinting = true;
             Category = "strict";
           };
+          # ---- EXTENSIONS ----
+          # Check about:support for extension/add-on ID strings.
+          # Valid strings for installation_mode are "allowed", "blocked",
+          # "force_installed" and "normal_installed".
+          ExtensionSettings = {
+            # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+            # uBlock Origin:
+            "uBlock0@raymondhill.net" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # Privacy Badger:
+            # "jid1-MnnxcxisBPnSXQ@jetpack" = {
+            #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+            #   installation_mode = "force_installed";
+            # };
+            # 1Password:
+            "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # DuckDuckGo
+            "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/jid1-ZAdIEUB7XOzOJw@jetpack/latest.xpi";
+              installation_mode = "force_installed";
+            };
+          };
         };
       };
 
