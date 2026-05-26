84248f8e30cd6934a339ebb8b1a28e4bd60e30e1
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-25 20:13:00 -0700

    Fix: incorrect closing parens position - firefox

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -65,76 +65,76 @@
             };
           };
         };
+
+        # Profile Config
+        profiles.default = {
+
+          # bookmarks = self.homeModules.fire-bookmarks;
+          # Search Engines
+          search = {
+            force = true;
+            default = "DuckDuckGo";
+            privateDefault = "DuckDuckGo";
+
+            engines = {
+              "Nix Packages" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      {
+                        name = "channel";
+                        value = "unstable";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@np" ];
+              };
+
+              "Nix Options" = {
+                urls = [
+                  {
+                    template = "https://search.nixos.org/options";
+                    params = [
+                      {
+                        name = "channel";
+                        value = "unstable";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@no" ];
+              };
+
+              "NixOS Wiki" = {
+                urls = [
+                  {
+                    template = "https://wiki.nixos.org/w/index.php";
+                    params = [
+                      {
+                        name = "search";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@nw" ];
+              };
+            };
+          };
+        };
       };
-
-      # Profile Config
-      # profiles.default = {
-
-      #   # bookmarks = self.homeModules.fire-bookmarks;
-      #   # Search Engines
-      #   search = {
-      #     force = true;
-      #     default = "DuckDuckGo";
-      #     privateDefault = "DuckDuckGo";
-
-      #     engines = {
-      #       "Nix Packages" = {
-      #         urls = [
-      #           {
-      #             template = "https://search.nixos.org/packages";
-      #             params = [
-      #               {
-      #                 name = "channel";
-      #                 value = "unstable";
-      #               }
-      #               {
-      #                 name = "query";
-      #                 value = "{searchTerms}";
-      #               }
-      #             ];
-      #           }
-      #         ];
-      #         icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      #         definedAliases = [ "@np" ];
-      #       };
-
-      #       "Nix Options" = {
-      #         urls = [
-      #           {
-      #             template = "https://search.nixos.org/options";
-      #             params = [
-      #               {
-      #                 name = "channel";
-      #                 value = "unstable";
-      #               }
-      #               {
-      #                 name = "query";
-      #                 value = "{searchTerms}";
-      #               }
-      #             ];
-      #           }
-      #         ];
-      #         icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      #         definedAliases = [ "@no" ];
-      #       };
-
-      #       "NixOS Wiki" = {
-      #         urls = [
-      #           {
-      #             template = "https://wiki.nixos.org/w/index.php";
-      #             params = [
-      #               {
-      #                 name = "search";
-      #                 value = "{searchTerms}";
-      #               }
-      #             ];
-      #           }
-      #         ];
-      #         icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      #         definedAliases = [ "@nw" ];
-      #       };
-      #     };
-      # };
-      # };
     };
 }
