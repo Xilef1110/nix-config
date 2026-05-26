14ff3328c5276b057535bb88227984c26c1f2810
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-25 22:05:06 -0700

    Fix: firefox bookmarks

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -86,12 +86,86 @@
             force = true;
             settings = [
               {
+                name = "Mozilla Firefox";
+                bookmarks = [
+                  {
+                    name = "Get Help";
+                    url = "https://support.mozilla.org/products/firefox";
+                  }
+                  {
+                    name = "Customize Firefox";
+                    url = "https://support.mozilla.org/kb/customize-firefox-controls-buttons-and-toolbars?utm_source=firefox-browser&utm_medium=default-bookmarks&utm_campaign=customize";
+                  }
+                  {
+                    name = "Get Involved";
+                    url = "https://www.mozilla.org/contribute/";
+                  }
+                  {
+                    name = "About Us";
+                    url = "https://www.mozilla.org/about/";
+                  }
+                ];
+              }
+              {
                 name = "kernel.org";
                 url = "https://www.kernel.org";
               }
               {
-                name = "nixconf/wrappedPrograms/niri.nix at main · vimjoyer/nixconf";
-                url = "https://github.com/vimjoyer/nixconf/blob/main/wrappedPrograms/niri.nix";
+                name = "nix/niri";
+                bookmarks = [
+                  {
+                    name = "dotfriedrice/.config/niri/config.kdl at master · nickjj/dotfriedrice";
+                    url = "https://github.com/nickjj/dotfriedrice/blob/master/.config/niri/config.kdl";
+                  }
+                  {
+                    name = "nixconf/wrappedPrograms/niri.nix at main · vimjoyer/nixconf";
+                    url = "https://github.com/vimjoyer/nixconf/blob/main/wrappedPrograms/niri.nix";
+                  }
+                ];
+              }
+              {
+                name = "UBC";
+                bookmarks = [
+                  {
+                    name = "Home - Workday";
+                    url = "https://wd10.myworkday.com/ubc/d/home.htmld";
+                  }
+                ];
+              }
+              {
+                name = "Excel";
+                bookmarks = [
+                  {
+                    name = "Athlete Resources - Exceleration Triathlon and Multisport";
+                    url = "https://excelerationtriclub.ca/youth-junior-u23-resource-page/";
+                  }
+                  {
+                    name = "Airtable - Run_Paces";
+                    url = "https://airtable.com/appbItJyrDbCbkKOb/shrJsnrz1LMLqxbV4/tblnb69JKZ7zUQX3z";
+                  }
+                  {
+                    name = "Airtable - Swim_Paces";
+                    url = "https://airtable.com/appbItJyrDbCbkKOb/shr1pSH7tztolaF3k/tblnb69JKZ7zUQX3z";
+                  }
+                  {
+                    name = "TrainingPeaks - Plan your training, track your workouts and measure your progress";
+                    url = "https://app.trainingpeaks.com/#calendar";
+                  }
+                ];
+              }
+              {
+                name = "Bookmarks Toolbar";
+                toolbar = true;
+                bookmarks = [
+                  {
+                    name = "Play board games online! • Board Game Arena";
+                    url = "https://boardgamearena.com/";
+                  }
+                  {
+                    name = "NixOS Search - Packages";
+                    url = "https://search.nixos.org/packages";
+                  }
+                ];
               }
             ];
           };
