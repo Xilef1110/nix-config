7a6363d78f25ef09a68a6624e33feb9ba27c3024
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-05-25 19:56:50 -0700

    Add: language pack, privacy badger

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -5,7 +5,7 @@
     {
       programs.firefox = {
         enable = true;
-        languagePack = [
+        languagePacks = [
           "en-CA"
           "en-US"
         ];
@@ -49,10 +49,10 @@
               installation_mode = "force_installed";
             };
             # Privacy Badger:
-            # "jid1-MnnxcxisBPnSXQ@jetpack" = {
-            #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-            #   installation_mode = "force_installed";
-            # };
+            "jid1-MnnxcxisBPnSXQ@jetpack" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+              installation_mode = "force_installed";
+            };
             # 1Password:
             "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
