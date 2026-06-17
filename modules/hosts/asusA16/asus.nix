8fab1dc80dc92331db6e8a4335ca15093f12531a
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-06-17 15:24:56 -0700

    Add: Shared configuration for Asus laptop

---
+++ modules/hosts/asusA16/asus.nix
@@ -0,0 +1,89 @@
+{ self, inputs, ... }:
+{
+  flake.nixosModule.asusShared =
+    { config, pkgs, ... }:
+    {
+
+      # Bootloader.
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+
+      # Use latest kernel.
+      boot.kernelPackages = pkgs.linuxPackages_latest;
+
+      networking.hostName = "nixos"; # Define your hostname.
+      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+      # Enable networking
+      networking.networkmanager.enable = true;
+
+      # Set your time zone.
+      time.timeZone = "America/Vancouver";
+
+      # Select internationalisation properties.
+      i18n.defaultLocale = "en_CA.UTF-8";
+
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_CA.UTF-8";
+        LC_IDENTIFICATION = "en_CA.UTF-8";
+        LC_MEASUREMENT = "en_CA.UTF-8";
+        LC_MONETARY = "en_CA.UTF-8";
+        LC_NAME = "en_CA.UTF-8";
+        LC_NUMERIC = "en_CA.UTF-8";
+        LC_PAPER = "en_CA.UTF-8";
+        LC_TELEPHONE = "en_CA.UTF-8";
+        LC_TIME = "en_CA.UTF-8";
+      };
+
+      # Enable touchpad support (enabled default in most desktopManager).
+      services.xserver.libinput.enable = true;
+      services.asusd = {
+        enable = true;
+      };
+
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      home-manager.users.felix = self.homeModules.asusHomeModule;
+
+      # Allow unfree packages
+      nixpkgs.config.allowUnfree = true;
+
+      users.users."felix" = {
+        isNormalUser = true;
+        description = "Felix";
+        extraGroups = [
+          "networkmanager"
+          "wheel"
+        ];
+      };
+
+      # Enable the X11 windowing system.
+      services.xserver.enable = true;
+      # Configure keymap in X11
+      services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+      };
+
+      # Enable CUPS to print documents.
+      services.printing.enable = true;
+
+      # Enable sound with pipewire.
+      services.pulseaudio.enable = false;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+      };
+    };
+}
--- modules/hosts/asusA16/gnome/configuration.nix
+++ modules/hosts/asusA16/gnome/configuration.nix
@@ -11,91 +11,12 @@
         self.nixosModules.packages
         self.nixosModules.communication
         self.nixosModules.system
+        self.nixosModules.asusShared
       ];
-
-      # Bootloader.
-      boot.loader.systemd-boot.enable = true;
-      boot.loader.efi.canTouchEfiVariables = true;
-
-      # Use latest kernel.
-      boot.kernelPackages = pkgs.linuxPackages_latest;
-
-      networking.hostName = "nixos"; # Define your hostname.
-      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-      # Configure network proxy if necessary
-      # networking.proxy.default = "http://user:password@proxy:port/";
-      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-      # Enable networking
-      networking.networkmanager.enable = true;
-
-      # Set your time zone.
-      time.timeZone = "America/Vancouver";
-
-      # Select internationalisation properties.
-      i18n.defaultLocale = "en_CA.UTF-8";
-
-      i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_CA.UTF-8";
-        LC_IDENTIFICATION = "en_CA.UTF-8";
-        LC_MEASUREMENT = "en_CA.UTF-8";
-        LC_MONETARY = "en_CA.UTF-8";
-        LC_NAME = "en_CA.UTF-8";
-        LC_NUMERIC = "en_CA.UTF-8";
-        LC_PAPER = "en_CA.UTF-8";
-        LC_TELEPHONE = "en_CA.UTF-8";
-        LC_TIME = "en_CA.UTF-8";
-      };
-
-      # Enable the X11 windowing system.
-      services.xserver.enable = true;
 
       # Enable the GNOME Desktop Environment.
       services.displayManager.gdm.enable = true;
       services.desktopManager.gnome.enable = true;
-
-      # Configure keymap in X11
-      services.xserver.xkb = {
-        layout = "us";
-        variant = "";
-      };
-
-      # Enable CUPS to print documents.
-      services.printing.enable = true;
-
-      # Enable sound with pipewire.
-      services.pulseaudio.enable = false;
-      security.rtkit.enable = true;
-      services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
-
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
-      };
-
-      # Enable touchpad support (enabled default in most desktopManager).
-      # services.xserver.libinput.enable = true;
-
-      # Define a user account. Don't forget to set a password with ‘passwd’.
-      users.users."felix" = {
-        isNormalUser = true;
-        description = "Felix";
-        extraGroups = [
-          "networkmanager"
-          "wheel"
-        ];
-      };
-      home-manager.users.felix = self.homeModules.asusHomeModule;
-
-      # Allow unfree packages
-      nixpkgs.config.allowUnfree = true;
 
       # List packages installed in system profile. To search, run:
       # $ nix search wget
@@ -123,14 +44,6 @@
         iagno # go game
         tali # poker game
         totem # video player
-      ];
-      services.asusd = {
-        enable = true;
-      };
-
-      nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
       ];
       # Before changing this value read the documentation for this option
       # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
