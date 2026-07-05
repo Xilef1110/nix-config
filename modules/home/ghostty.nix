c67ad0e96408c5ce0592aae294afac4fa29e11d8
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-07-05 08:35:59 -0700

    Add: ghostty config

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -7,6 +7,27 @@
       programs.ghostty = {
         enable = true;
         settings = {
+          theme = "Adwaita Dark"; # Nordfox, Catpuccin Mocha
+          background-opacity = "0.95";
+          background-blur = "16";
+          confirm-close-surface = true;
+          keybind = [
+            # tabs
+            "ctrl+t=new_tab"
+            "super+shift+l=next_tab"
+            "super+shift+h=previous_tab"
+            # splits
+            "ctrl+shift+l=new_split:right"
+            "ctrl+shift+h=new_split:left"
+            "ctrl+shift+j=new_split:down"
+            "ctrl+shift+k=new_split:up"
+            "ctrl+l=goto_split:right"
+            "ctrl+h=goto_split:left"
+            "ctrl+j=goto_split:down"
+            "ctrl+k=goto_split:up"
+            # general
+            "ctrl+w=close_surface"
+          ];
         };
       };
     };
