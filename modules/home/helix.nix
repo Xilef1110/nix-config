0e128688e16964a174ded3b02fe6fc8b985e3a2f
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-07-19 19:17:36 -0700

    Add: python config for helix

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -26,19 +26,42 @@
             auto-format = true;
             # formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
           }
-        ];
-        languages.language-server.tinymist = {
-          command = "tinymist";
-          config = {
-            exportPdf = "onSave";
-            preview.background.enabled = true;
-            preview.background.args = [
-              "--data-plane-host=127.0.0.1:0"
-              "--invert-colors=never"
-              "--open"
+          {
+            name = "pyton";
+            auto-format = true;
+            language-servers = [
+              {
+                name = "ty";
+                except-features = [
+                  "goto-definition"
+                  "goto-declaration"
+                  "goto-type-definition"
+                  "goto-reference"
+                  "goto-implementation"
+                  "document-symbols"
+                  "workspace-symbols"
+                ];
+              }
+              "ruff"
+              "jedi"
             ];
+          }
+        ];
+        languages.language-server = {
+          tinymist = {
+            command = "tinymist";
+            config = {
+              exportPdf = "onSave";
+              preview.background.enabled = true;
+              preview.background.args = [
+                "--data-plane-host=127.0.0.1:0"
+                "--invert-colors=never"
+                "--open"
+              ];
+            };
           };
         };
+
       };
     };
 }
