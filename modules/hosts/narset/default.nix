3ffc21ae3a9ebae2533ca4091265f72f0a058b86
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-09-10 20:29:57 -0700

    Fix: narset home entrypoint

--- modules/hosts/narset/default.nix
+++ modules/hosts/narset/default.nix
@@ -1,5 +1,15 @@
 { self, inputs, ... }:
 {
+  flake.homeConfigurations.Narset = inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+    modules = [
+      self.nixosModules.narsetConfiguration
+      {
+        home.username = "felix";
+        home.homeDirectory = "/home/felix";
+      }
+    ];
+  };
   flake.nixosConfigurations.narset = inputs.nixpkgs.lib.nixosSystem {
     modules = [
       self.nixosModules.narsetConfiguration
